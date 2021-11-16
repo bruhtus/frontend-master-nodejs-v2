9b8c5b8067093bd4ddda2b4574ce473f05716de4
bruhtus <hibruhtus@gmail.com>
2021-11-16 10:07:41 +0700

    feat(fs module): add basic implementation of fs module
    note:
    what this changes basically do was read the `template.html` file,
    and then changed the `{title}` and `{body}` to `learn node.js` and
    `this is the final html` respectively. finally, write the output to
    `index.html` file.

--- app/index.mjs
+++ app/index.mjs
@@ -1,3 +1,18 @@
-import fs from 'fs'
+import { readFile, writeFile } from 'fs/promises'
 
-console.log(fs)
+// console.log(import.meta.url)
+
+// wait until this function finish
+let template = await readFile(new URL('template.html', import.meta.url), 'utf-8')
+
+const data = {
+  title: 'learn node.js',
+  body: 'this is the final html'
+}
+
+for (const [key, value] of Object.entries(data)) {
+  template = template.replace(`{${key}}`, value)
+}
+
+// wait until this function finish
+await writeFile(new URL('index.html', import.meta.url), template)
