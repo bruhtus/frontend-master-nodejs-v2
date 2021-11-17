55c47b482ccf7fcfc793ccd93387563f55f6ab00
bruhtus <hibruhtus@gmail.com>
2021-11-17 10:56:35 +0700

    feat(server): create server with express node module
    note:
    instead of using the internal module such as `http`, in this changes
    we use express module to create a simple server.

---
+++ app/server/server.mjs
@@ -0,0 +1,32 @@
+import express from 'express'
+import bp from 'body-parser'
+import morgan from 'morgan'
+
+const app = express()
+
+// if someone parses some data on a query string, it's gonna parse that for us
+// and it basically just make sure that we can parse the query string
+// and whatever is in the URL pretty easily
+app.use(bp.urlencoded({extended: true}))
+app.use(bp.json())
+app.use(morgan('dev'))
+
+const db = []
+
+app.post('/todo', (request, response) => {
+  const newTodo = {
+    id: Date.now(),
+    text: request.body.text
+  }
+
+  db.push(newTodo)
+  response.json(newTodo)
+})
+
+app.get('/todo', (request, response) => {
+  response.json(db)
+})
+
+app.listen(8000, () => {
+  console.log('Server on https://localhost:8000')
+})
